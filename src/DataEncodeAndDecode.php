225d4c9d99210dc438a15cd2d5bba23df5341f2e
linjuchen <cmmia@163.com>
2019-07-22 16:03:57 +0800

    双因子

--- src/DataEncodeAndDecode.php
+++ src/DataEncodeAndDecode.php
@@ -14,7 +14,11 @@ class DataEncodeAndDecode{
 		$encrypted = openssl_encrypt($text, 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
 		return array(base64_encode($encrypted), md5($this->iv."+_+".$text));
 	}
-	public function decode($text){
-		return openssl_decrypt(base64_decode($text), 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
+	public function decode($text,$hash){
+		$text = openssl_decrypt(base64_decode($text), 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
+		if(md5($this->iv."+_+".$text) !== $hash){
+			return 0;
+		}
+		return $text;
 	}
 }
