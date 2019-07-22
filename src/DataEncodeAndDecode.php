5d263909a19e7b2d9a83e0e9b321070397affbc2
linjuchen <cmmia@163.com>
2019-07-22 13:31:26 +0800

    init

--- src/DataEncodeAndDecode.php
+++ src/DataEncodeAndDecode.php
@@ -12,7 +12,7 @@ class DataEncodeAndDecode{
 	
 	public function encode($text){
 		$encrypted = openssl_encrypt($text, 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
-		return array(base64_encode($encrypted), $iv);
+		return array(base64_encode($encrypted), $this->iv);
 	}
 	public function decode($text){
 		return openssl_decrypt(base64_decode($text), 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
